162179f6b56c8417d42f229809ace597c9853f5b
Mehedi Sayed <sayedmehedi.aiub@gmail.com>
2021-03-07 10:38:07 +0600

    pdf viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdfViewerAngular';
+  constructor() {
+    pdfDefaultOptions.assetsFolder = 'assets';
+  }
 }
